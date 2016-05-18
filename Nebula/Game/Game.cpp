20f2999afef5b68a6299d24b454a39cb6a92dc65
Amazong <jdinster@gmail.com>
2016-05-18 18:01:15 +0100

    Testing SFML

--- Nebula/Game/Game.cpp
+++ Nebula/Game/Game.cpp
@@ -1,3 +1,12 @@
+#include "SFML\Graphics.hpp"
+
 int main() {
+	sf::RenderWindow window(sf::VideoMode(1618, 1000), "Working!");
+
+	while (window.isOpen()) {
+		window.clear();
+		window.display();
+	}
+
 	return(0);
 }
